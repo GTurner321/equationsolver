261c114998d6b154048467a622925533449bd7e7
GTurner321 <george.turner@jumeirahcollege.com>
2025-02-11 10:49:31 +0400

    Update phase1-commands.js

--- phase1-commands.js
+++ phase1-commands.js
@@ -1,1 +1,6 @@
-
+// phase1-commands.js
+function processPhase1Command(command, currentValues) {
+  // Try fraction equality command first (from common-commands.js)
+  if (processFractionEquality(command, currentValues)) {
+    return true;
+  }
