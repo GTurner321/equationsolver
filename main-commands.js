ecb45aab15747864558d6f1719a2ef2814a33db9
GTurner321 <george.turner@jumeirahcollege.com>
2025-02-11 11:20:53 +0400

    Update main-commands.js

--- main-commands.js
+++ main-commands.js
@@ -1,5 +1,8 @@
-// main-commands.js (new main command processing file)
-function processCommand(command, currentValues) {
+// main-commands.js
+function processCommand(command) {
+  // currentValues is accessed as a global variable
+  if (!command || typeof command !== 'string') return false;
+  
   // Determine which phase we're in
   const isPhase1 = evaluateFraction(currentValues.a, currentValues.b) !== 1 || 
                    evaluateFraction(currentValues.g, currentValues.h) !== 1;
